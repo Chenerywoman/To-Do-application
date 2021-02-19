308a193c7b1767add5e210f4c23c2568460daa71
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-19 09:51:32 +0000

    following code-along to create a deleteTask function to update the state of the task list in state

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,11 +1,12 @@
 import './Task.css'
 
-const Task = ({text, complete}) => {
+const Task = ({ id, text, complete, deleteTask }) => {
+
     return (
         <li className={`task ${complete ? 'task--complete' : ''}`}>
             <p className="task__text">{text}</p>
             {complete === false && <button className="button">Complete</button>}
-            <button className="button">Delete</button>
+            <button onClick={() => deleteTask(id)} className="button">Delete</button>
         </li>
     )
 }
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Task from '../Task/Task';
 
 
-const TaskList = ({tasks, status}) => {
+const TaskList = ({tasks, status, deleteTask}) => {
     return (
         <section className={`${status}-tasks`}>
             <h2 className="heading">{status === "incomplete" ? "Tasks to do:" : "Completed Tasks"}</h2>
             <ul className="task-list">
-                {tasks.map(task => <Task text={task.text} key={task.id} complete={task.completed}/>)}
+                {tasks.map(task => 
+                    <Task 
+                    id={task.id}
+                    text={task.text} 
+                    key={task.id} 
+                    complete={task.completed}
+                    deleteTask={ deleteTask }
+                    />)}
             </ul>
         </section>
     )
