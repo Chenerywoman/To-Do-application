b726536c3563f2c119206c47cdc92bf433309448
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-17 19:30:33 +0000

    adding useEffect with get request to tasks endpoint & updating references in code to match properties names in returned data

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from './components/Header/Header';
 import TaskList from './components/TaskList/TaskList';
 import './App.css';
 
 function App() {
 
-    const [tasks, setTasks] = useState([
-        {text: 'Buy cat food', completed: false, date: '2020-12-01', id: '001'}, {text: 'Buy milk', completed: true, date: '2020-12-05', id:'002'},
-        {text: 'Clean the kitchen', completed: false, date: '2020-11-', id:'003'},
-        {text: 'Go to sleep', completed: true, date: '2020-09-18', id:'004'},
-        {text: 'Go for a walk', completed: false, date: '2020-09-18', id:'005'},
-        {text: 'Do the food shop', completed: false, date: '2020-09-18', id:'006'},
-    ]); 
+  // const [tasks, setTasks] = useState([
+  //     {text: 'Buy cat food', completed: false, date: '2020-12-01', id: '001'}, {text: 'Buy milk', completed: true, date: '2020-12-05', id:'002'},
+  //     {text: 'Clean the kitchen', completed: false, date: '2020-11-', id:'003'},
+  //     {text: 'Go to sleep', completed: true, date: '2020-09-18', id:'004'},
+  //     {text: 'Go for a walk', completed: false, date: '2020-09-18', id:'005'},
+  //     {text: 'Do the food shop', completed: false, date: '2020-09-18', id:'006'},
+  // ]); 
 
-    const deleteTask = id => {
+  const [tasks, setTasks] = useState([]);
 
-      const updatedTasks = tasks.filter(task => task.id !== id);
+  useEffect(() => {
+    // initiate a get request to api endpoint
+    axios.get('https://qyyetfu7di.execute-api.eu-west-2.amazonaws.com/dev/users/299982cb-82aa-4a7b-9fef-e92fd7abd41b/tasks')
+      // promise resolves - update state of tasks
+      .then(response => setTasks(response.data))
+      // if error - log out error
+      .catch(err => console.log(err))
+  }, [])
 
-      setTasks(updatedTasks);
+  const deleteTask = id => {
 
+    const updatedTasks = tasks.filter(task => task.taskId !== id);
+
+    setTasks(updatedTasks);
+
+  }
+
+  const addTask = text => {
+
+    const newTask = {
+      description: text,
+      completed: false,
+      taskId: '007'
     }
 
-    const addTask = text => {
-
-      const newTask = {
-        text: text, 
-        completed: false,
-        date: '2021-12-01',
-        id: '007'
-      }
-
-      const updatedTasks = [ ...tasks, newTask];
-      setTasks(updatedTasks);
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
 
 
-    }
-    const incompleteTasks = tasks.filter(task => !task.completed);
-    const completeTasks = tasks.filter(task => task.completed);
+  }
+  const incompleteTasks = tasks.filter(task => !task.completed);
+  const completeTasks = tasks.filter(task => task.completed);
 
   return (
     <div className='App'>
-    < Header taskCount={incompleteTasks.length} addTask={ addTask }/>
-    <main className='all-tasks'>
-       <TaskList deleteTask={ deleteTask } tasks={incompleteTasks} status='incomplete'/>
-       <TaskList deleteTask={ deleteTask } tasks={completeTasks} status='complete'/>
-    </main>
+      < Header taskCount={incompleteTasks.length} addTask={addTask} />
+      <main className='all-tasks'>
+        <TaskList deleteTask={deleteTask} tasks={incompleteTasks} status='incomplete' />
+        <TaskList deleteTask={deleteTask} tasks={completeTasks} status='complete' />
+      </main>
     </div>
   );
 }
