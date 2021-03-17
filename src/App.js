2e3d4961300137776d2f75d71357656b6ff2d7ea
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-17 20:53:34 +0000

    Removing console.logs

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import './App.css';
 
 function App() {
 
-  // const [tasks, setTasks] = useState([
-  //     {text: 'Buy cat food', completed: false, date: '2020-12-01', id: '001'}, {text: 'Buy milk', completed: true, date: '2020-12-05', id:'002'},
-  //     {text: 'Clean the kitchen', completed: false, date: '2020-11-', id:'003'},
-  //     {text: 'Go to sleep', completed: true, date: '2020-09-18', id:'004'},
-  //     {text: 'Go for a walk', completed: false, date: '2020-09-18', id:'005'},
-  //     {text: 'Do the food shop', completed: false, date: '2020-09-18', id:'006'},
-  // ]); 
-
   const [tasks, setTasks] = useState([]);
   const userId = '299982cb-82aa-4a7b-9fef-e92fd7abd41b';
 
@@ -41,14 +33,9 @@ function App() {
       completed: false,
     }
 
-    // make a post request & pass in new task as the body
-
     axios.post(`https://qyyetfu7di.execute-api.eu-west-2.amazonaws.com/dev/users/${userId}/tasks`, newTask)
-      // if get request is successful, add a get request for all the tasks
       .then(() => axios.get(`https://qyyetfu7di.execute-api.eu-west-2.amazonaws.com/dev/users/${userId}/tasks`))
-      // if get request is successful update tasks state will tasks array
       .then(response => setTasks(response.data))
-      // if error, log out the error
       .catch(error => console.log(error))
   }
 
