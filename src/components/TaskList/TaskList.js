6398acc5e74973ae606dd8f72c42ec20a1b81398
Chenerywoman <rachelmchenery@hotmail.com>
2021-03-17 19:32:21 +0000

    updating code to match property names received from backend

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Task from '../Task/Task';
 
 
-const TaskList = ({tasks, status, deleteTask}) => {
+const TaskList = ({ tasks, status, deleteTask }) => {
     return (
         <section className={`${status}-tasks`}>
             <h2 className='heading'>{status === 'incomplete' ? 'Tasks to do:' : 'Completed Tasks'}</h2>
             <ul className='task-list'>
-                {tasks.map(task => 
-                    <Task 
-                    id={task.id}
-                    text={task.text} 
-                    key={task.id} 
-                    complete={task.completed}
-                    deleteTask={ deleteTask }
+                {tasks.map(task =>
+                    <Task
+                        id={task.taskId}
+                        text={task.description}
+                        key={task.taskId}
+                        complete={task.completed}
+                        deleteTask={deleteTask}
                     />)}
             </ul>
         </section>
